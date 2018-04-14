aa5438e8061a22a9ad7301693ab1a555c3a0eb05
Sammy Joseph <samuelsadiq52@gmail.com>
2018-04-14 15:40:04 +0100

    Updated files

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,17 @@ module Articleapi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.action_dispatch.default_headers.merge!({
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => '*'
-    })
+
+      config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins '*'
+          resource (
+            '*', 
+            headers: => :any, 
+            methods: => [:get, :patch, :put, :delete, :post, :options]
+            )
+        end
+      end
+    end
   end
 end

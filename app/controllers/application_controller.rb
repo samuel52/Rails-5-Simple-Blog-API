ce2b6b7896f17be6ecb55e2247310d58c22c3859
Sammy Joseph <samuelsadiq52@gmail.com>
2018-04-14 14:38:56 +0100

    Add file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::API
 
-	config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
-
 end
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://safe-coast-54130.herokuapp.com',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
+
+  
 module Articleapi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.

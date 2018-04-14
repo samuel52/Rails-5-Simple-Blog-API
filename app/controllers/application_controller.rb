af2612b5678d0d46b84a7a41d7463392e152e8c8
Sammy Joseph <samuelsadiq52@gmail.com>
2018-04-14 15:25:19 +0100

    Updated files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::API
 
-	config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:3000', /https*:\/\/.*?chompoo500ml.github\.io/
-        resource '*', :headers => :any, :methods => :any
-      end
-    end
-
 end
